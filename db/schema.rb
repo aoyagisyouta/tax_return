fae606415ff3bf6e8d9937b0f643039613da8651
aoyagisyouta <xiangtaiqingliu06@gmail.com>
2024-09-03 12:01:17 +0900

    Propertyモデル実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_01_065322) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_03_024804) do
+  create_table "properties", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "street_number", null: false
+    t.string "room_number"
+    t.integer "building_type_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_01_065322) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "properties", "users"
 end
