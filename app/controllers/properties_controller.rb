d504c1663c82d1f0a695113a6f04d0ec7c8b4fbc
aoyagisyouta <xiangtaiqingliu06@gmail.com>
2024-09-08 18:09:55 +0900

    削除機能実装

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,5 @@
 class PropertiesController < ApplicationController
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
   def index
     @properties = Property.all 
   end
@@ -17,12 +18,15 @@ class PropertiesController < ApplicationController
   end
 
   def edit 
-    @property = Property.find(params[:id])
   end
 
   def update
-    property = Property.find(params[:id])
-    property.update(property_params)
+    @property.update(property_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @property.destroy
     redirect_to root_path
   end
 
@@ -30,5 +34,9 @@ class PropertiesController < ApplicationController
   def property_params
     params.require(:property).permit(:name, :postal_code, :prefecture_id,
     :city, :street_number, :room_number, :building_type_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_property 
+    @property = Property.find(params[:id])
   end
 end
