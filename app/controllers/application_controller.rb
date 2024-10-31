e70de8b651e3db7e1262f686218dbccf55cbf597
aoyagisyouta <xiangtaiqingliu06@gmail.com>
2024-10-31 16:59:39 +0900

    basic設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   protected
 
@@ -12,5 +13,13 @@ class ApplicationController < ActionController::Base
 
     flash[:alert] = 'アクセス権限がありません。'
     redirect_to root_path
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD'] # 環境変数を読み込む記述に変更
+    end
   end
 end
